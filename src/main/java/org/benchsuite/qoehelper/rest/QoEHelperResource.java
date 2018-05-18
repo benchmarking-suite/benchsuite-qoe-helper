afa76e9c10fbbcb9b6c968f08dfd63684cb691e7
Gabriele Giammatteo <gabriele.giammatteo@eng.it>
2018-05-18 16:18:21 +0200

    first design of the service model

--- src/main/java/org/benchsuite/qoehelper/rest/QoEHelperResource.java
+++ src/main/java/org/benchsuite/qoehelper/rest/QoEHelperResource.java
@@ -1,5 +1,26 @@
 package org.benchsuite.qoehelper.rest;
 
+import org.benchsuite.qoehelper.QoEHelper;
+import org.benchsuite.qoehelper.model.CloudInfo;
+
+import java.util.Map;
+
 public class QoEHelperResource {
+
+  private QoEHelper service;
+
+  public QoEHelperResource(){
+    this.service = new QoEHelper();
+  }
+
+
+  public CloudInfo getCloudInfo(
+      String provider,
+      String identity,
+      String credentials,
+      Map<String, String> optionalParameters){
+
+    return this.service.getCloudInfo(provider, identity, credentials, optionalParameters);
+  }
 
 }
