317897266c28ba9668b64ccb6a42b58029c744e6
Gabriele Giammatteo <gabriele.giammatteo@eng.it>
2018-06-07 14:19:58 +0200

    created testing client

--- src/test/java/org/benchsuite/qoehelper/Client.java
+++ src/test/java/org/benchsuite/qoehelper/Client.java
@@ -1,31 +1,41 @@
 package org.benchsuite.qoehelper;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.InputStream;
 
 import org.benchsuite.qoehelper.model.GetInfoRequest;
 
 public class Client {
 	
-	public static void main(String[] args){
-		
+	public static void main(String[] args) throws IOException {
+
+		Properties props = new Properties();
+		InputStream in = new FileInputStream("/home/ggiammat/test/qoe-helper/test.properties");
+		props.load(in);
+		in.close();
+
 		javax.ws.rs.client.Client client = ClientBuilder.newClient();
 		
 		WebTarget target = client.target("http://localhost:8080/benchsuite-qoe_helper-0.0.1-SNAPSHOT/rest").path("CloudInfo");
 		
 		GetInfoRequest iData= new GetInfoRequest();
-		iData.setProvider("");
-		iData.setIdentity("");
-		iData.setCredentials("");
+		iData.setProvider("openstack-nova");
+		System.out.println(props.getProperty("fiware_user"));
+		iData.setIdentity(props.getProperty("fiware_user"));
+		iData.setCredentials(props.getProperty("fiware_pass"));
 		Map<String, String> optionalParameters = new HashMap<String,String>();
-		optionalParameters.put("host", "");
-		optionalParameters.put("region", "");
+		optionalParameters.put("host", props.getProperty("fiware_host"));
+		optionalParameters.put("region", props.getProperty("fiware_region"));
 		iData.setOptionalParameters(optionalParameters);
 		
 		Response response = target.request().post(Entity.entity(iData, MediaType.APPLICATION_JSON)); 
