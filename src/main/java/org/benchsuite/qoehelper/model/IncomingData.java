21b07c18f5e717ce6a71a728b4ab00ec7ae4a200
vincenzo terrano <vincenzo.terrano@eng.it>
2018-06-07 12:17:30 +0200

    Revert "Revert "Commit 2 - Initial Rest""
    This reverts commit 62508b44f07478ad991dc2d5014a9f3769c77d5b.

---
+++ src/main/java/org/benchsuite/qoehelper/model/IncomingData.java
@@ -0,0 +1,37 @@
+package org.benchsuite.qoehelper.model;
+
+import java.util.Map;
+
+public class IncomingData {
+	
+	private String provider;
+	private String identity;
+	private String credentials;
+	private Map<String, String> optionalParameters;
+	
+	public String getProvider() {
+		return provider;
+	}
+	public void setProvider(String provider) {
+		this.provider = provider;
+	}
+	public String getIdentity() {
+		return identity;
+	}
+	public void setIdentity(String identity) {
+		this.identity = identity;
+	}
+	public String getCredentials() {
+		return credentials;
+	}
+	public void setCredentials(String credentials) {
+		this.credentials = credentials;
+	}
+	public Map<String, String> getOptionalParameters() {
+		return optionalParameters;
+	}
+	public void setOptionalParameters(Map<String, String> optionalParameters) {
+		this.optionalParameters = optionalParameters;
+	}
+
+}
---
+++ src/main/java/org/benchsuite/qoehelper/providers/JCloudsAmazon.java
@@ -0,0 +1,171 @@
+package org.benchsuite.qoehelper.providers;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableSet;
+import com.google.inject.Module;
+
+import org.jclouds.ContextBuilder;
+import org.jclouds.aws.ec2.AWSEC2Api;
+import org.jclouds.aws.ec2.compute.AWSEC2TemplateOptions;
+import org.jclouds.aws.ec2.features.AWSAMIApi;
+import org.jclouds.aws.ec2.features.AWSSecurityGroupApi;
+import org.jclouds.compute.ComputeServiceContext;
+import org.jclouds.compute.domain.ComputeMetadata;
+import org.jclouds.compute.domain.Hardware;
+import org.jclouds.compute.domain.Image;
+import org.jclouds.compute.domain.OsFamily;
+import org.jclouds.compute.domain.SecurityGroup;
+import org.jclouds.compute.domain.Template;
+import org.jclouds.compute.extensions.ImageExtension;
+import org.jclouds.compute.extensions.SecurityGroupExtension;
+import org.jclouds.domain.Location;
+import org.jclouds.logging.slf4j.config.SLF4JLoggingModule;
+import org.jclouds.sshj.config.SshjSshClientModule;
+import org.jclouds.ec2.EC2Api;
+import org.jclouds.ec2.compute.options.EC2TemplateOptions;
+import org.jclouds.aws.ec2.options.AWSDescribeImagesOptions;
+
+import org.jclouds.ec2.domain.Subnet;
+import org.jclouds.ec2.features.SecurityGroupApi;
+import org.jclouds.ec2.features.SubnetApi;
+
+import static org.jclouds.aws.ec2.reference.AWSEC2Constants.PROPERTY_EC2_AMI_QUERY;
+import static org.jclouds.aws.ec2.reference.AWSEC2Constants.PROPERTY_EC2_CC_AMI_QUERY;
+import static org.jclouds.compute.config.ComputeServiceProperties.TIMEOUT_SCRIPT_COMPLETE;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.Authenticator;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.benchsuite.qoehelper.model.*;
+import org.benchsuite.qoehelper.model.HardwareProfile;
+
+
+public class JCloudsAmazon implements Closeable {
+	private final ComputeServiceContext context;
+	private final EC2Api ec2Context;
+	private final AWSEC2Api awsec2Context;
+
+    public JCloudsAmazon(String identity,String credential) {
+        Iterable<Module> modules = ImmutableSet.<Module>of(new SshjSshClientModule(),
+                new SLF4JLoggingModule()
+                );
+        
+        Properties properties = new Properties();
+        properties.setProperty(PROPERTY_EC2_AMI_QUERY, "owner-id=137112412989;state=available;image-type=machine");
+        properties.setProperty(PROPERTY_EC2_CC_AMI_QUERY, "");
+        long scriptTimeout = TimeUnit.MILLISECONDS.convert(20, TimeUnit.MINUTES);
+        properties.setProperty(TIMEOUT_SCRIPT_COMPLETE, scriptTimeout + "");
+        
+        // get a context with ec2 that offers the portable ComputeService API
+        context = ContextBuilder.newBuilder("aws-ec2")
+                .credentials(identity, credential)
+                .overrides(properties)
+                //.modules(modules)
+                .buildView(ComputeServiceContext.class);
+        
+        ec2Context = context.unwrapApi(EC2Api.class);
+        awsec2Context = context.unwrapApi(AWSEC2Api.class);
+    }
+    
+    public Collection<org.benchsuite.qoehelper.model.SecurityGroup> listSecurityGroup(String region){
+    	
+    	Collection<org.benchsuite.qoehelper.model.SecurityGroup> listSecurityGroups = new ArrayList<org.benchsuite.qoehelper.model.SecurityGroup>();
+    	org.benchsuite.qoehelper.model.SecurityGroup securityGroup = new org.benchsuite.qoehelper.model.SecurityGroup();
+    	
+    	// Security Groups for Regions
+    	AWSSecurityGroupApi secGroups = awsec2Context.getSecurityGroupApi().get();
+    	for(org.jclouds.ec2.domain.SecurityGroup sGroups: secGroups.describeSecurityGroupsInRegion(region)){
+    		
+    		securityGroup.setId(sGroups.getId());
+    		securityGroup.setName(sGroups.getName());
+    		listSecurityGroups.add(securityGroup);
+    		
+    		System.out.println("SecurityGroup for region:"+sGroups);
+    	}
+    	
+    	// Security Groups All
+//    	SecurityGroupExtension securityGroups = context.getComputeService().getSecurityGroupExtension().get();
+//    	for(SecurityGroup group: securityGroups.listSecurityGroups()){
+//    		System.out.println("SecurityGroup: "+group);
+//    	}
+    	
+    	return listSecurityGroups;
+    }
+    
+    public Collection<Network> listNetwork(String region){
+    	
+    	Collection<Network> listNetworks = new ArrayList<Network>();
+    	Network network = new Network();
+    	   	
+    	//Subnet all with AWSEC2Api.class
+//    	SubnetApi subnetsAWSEC2Context = ec2Context.getSubnetApi().get();  
+//    	for(Subnet subnet: subnetsAWSEC2Context.list()){
+//    		System.out.println(""+subnet);
+//    	}
+    	
+    	// Subnet for region with AWSEC2Api.class
+    	SubnetApi subnetsRegion = awsec2Context.getSubnetApiForRegion(region).get();
+		for(Subnet subnetRegion: subnetsRegion.list()){
+			System.out.println(""+subnetRegion);
+			
+	    	network.setId(subnetRegion.getSubnetId());
+	    	network.setName(null);
+    		listNetworks.add(network);
+	    }
+	    
+    	return listNetworks;
+    }
+ 
+    public Collection<org.benchsuite.qoehelper.model.Image> listImages(String region){
+    	
+    	Collection<org.benchsuite.qoehelper.model.Image> listImages = new ArrayList<org.benchsuite.qoehelper.model.Image>();
+    	org.benchsuite.qoehelper.model.Image image = new org.benchsuite.qoehelper.model.Image();
+    	
+    	//System.out.println("test: "+awsec2Context.getAMIApi().get().describeImagesInRegion("ap-southeast-2"));
+    	
+    	Set<? extends Image> images = context.getComputeService().listImages(); 	
+    	for (Image i : images) {
+    		System.out.println("Images:  " + i);
+    		
+        	image.setId(i.getId());
+        	image.setName(i.getName());
+        	image.setDescription(i.getDescription());
+            listImages.add(image);        	
+        }
+                
+		return listImages;
+    }
+
+    public Collection<HardwareProfile> listHardwareProfiles(String region){
+    	
+    	Collection<HardwareProfile> listHardwareProfiles = new ArrayList<HardwareProfile>();
+    	HardwareProfile hardwareProfile = new HardwareProfile();
+    	    	
+    	Set<? extends Hardware> hardwareProfiles = context.getComputeService().listHardwareProfiles();
+    	for(Hardware h : hardwareProfiles){
+    		System.out.println("hardwareProfiles: "+ h.toString());
+    		
+    		hardwareProfile.setId(h.getId());
+    		hardwareProfile.setName(h.getName());
+    		hardwareProfile.setRam(h.getRam());
+    		hardwareProfile.setVcpus(h.getProcessors().get(0).getCores());
+    		listHardwareProfiles.add(hardwareProfile);
+    	}
+    	
+    	return listHardwareProfiles;
+    }
+        
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		context.close();
+	}
+
+}
