9dfbd10490f33ab4bd33a48d3ba8ed8fc4b9f37e
Gabriele Giammatteo <gabriele.giammatteo@eng.it>
2018-06-14 17:50:40 +0200

    removing testing class

--- src/test/java/org/benchsuite/qoehelper/Client.java
+++ src/test/java/org/benchsuite/qoehelper/Client.java
@@ -22,25 +22,25 @@ public class Client {
 	public static void main(String[] args) throws IOException, ProviderConfigurationException {
 
 		Properties props = new Properties();
-		InputStream in = new FileInputStream("C:/Users/vinterra/Desktop/test.properties.txt");
+		InputStream in = new FileInputStream("/home/ggiammat/test/qoe-helper/test.properties");
 		props.load(in);
 		in.close();
 
 		javax.ws.rs.client.Client client = ClientBuilder.newClient();
 		
-		WebTarget target = client.target("http://localhost:8080/benchsuite-qoe_helper/rest").path("CloudInfo");
+		WebTarget target = client.target("http://localhost:8080/qoehelper/rest").path("CloudInfo");
 		
 		GetInfoRequest iData= new GetInfoRequest();
 		
 		// Fiware OpenStack
-//		iData.setProvider("openstack");
-//		iData.setIdentity(props.getProperty("fiware_user"));
-//		iData.setCredentials(props.getProperty("fiware_pass"));
-//		Map<String, String> optionalParameters = new HashMap<String,String>();
-//		optionalParameters.put("authUrl", props.getProperty("fiware_authUrl"));
-//		optionalParameters.put("region", props.getProperty("fiware_region"));
-//		optionalParameters.put("project", props.getProperty("fiware_project"));
-//		iData.setOptionalParameters(optionalParameters);
+		iData.setProvider("openstack");
+		iData.setIdentity(props.getProperty("fiware_user"));
+		iData.setCredentials(props.getProperty("fiware_pass"));
+		Map<String, String> optionalParameters = new HashMap<String,String>();
+		optionalParameters.put("authUrl", props.getProperty("fiware_authUrl"));
+		optionalParameters.put("region", props.getProperty("fiware_region"));
+		optionalParameters.put("project", props.getProperty("fiware_project"));
+		iData.setOptionalParameters(optionalParameters);
 		
 		// Amazon AWS-EC2
 //		iData.setProvider("ec2");
@@ -51,14 +51,14 @@ public class Client {
 //		iData.setOptionalParameters(optionalParameters);
 		
 		// Cosmote OpenStack
-		iData.setProvider("openstack");
-		iData.setIdentity(props.getProperty("cosmote_user"));
-		iData.setCredentials(props.getProperty("cosmote_pass"));
-		Map<String, String> optionalParameters = new HashMap<String,String>();
-		optionalParameters.put("authUrl", props.getProperty("cosmote_authUrl"));
-		optionalParameters.put("region", props.getProperty("cosmote_region"));
-		optionalParameters.put("project", props.getProperty("cosmote_project"));
-		iData.setOptionalParameters(optionalParameters);
+//		iData.setProvider("openstack");
+//		iData.setIdentity(props.getProperty("cosmote_user"));
+//		iData.setCredentials(props.getProperty("cosmote_pass"));
+//		Map<String, String> optionalParameters = new HashMap<String,String>();
+//		optionalParameters.put("authUrl", props.getProperty("cosmote_authUrl"));
+//		optionalParameters.put("region", props.getProperty("cosmote_region"));
+//		optionalParameters.put("project", props.getProperty("cosmote_project"));
+//		iData.setOptionalParameters(optionalParameters);
 
 		Response response = target.request().post(Entity.entity(iData, MediaType.APPLICATION_JSON));
 		
