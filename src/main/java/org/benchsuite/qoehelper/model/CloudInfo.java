fa0192908def64a2c11a9a5714d1483f3ef0d21f
Gabriele Giammatteo <gabriele.giammatteo@eng.it>
2018-06-15 09:29:14 +0200

    copyright notice added to all source files

--- src/main/java/org/benchsuite/qoehelper/model/CloudInfo.java
+++ src/main/java/org/benchsuite/qoehelper/model/CloudInfo.java
@@ -1,5 +1,5 @@
 /* Benchmarking Suite
-   Copyright 2014-2018 Engineering Ingegneria Informatica S.p.A.
+   Copyright 2018 Engineering Ingegneria Informatica S.p.A.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
